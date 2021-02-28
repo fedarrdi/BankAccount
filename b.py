ea7cf7eeb4b589f717bb711c2a82e56302bee99a
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-02-28 07:41:21 +0200

    Add files via upload

--- b.py
+++ b.py
@@ -1,3 +1,6 @@
+import string
+import random 
+
 class Transaction:
     
     def __init__(self, index, type_, amount_withdraw_or_deposit, new_balance):
@@ -13,10 +16,10 @@ class Transaction:
         return self.__type
 
     def get_amount_withdraw_or_deposit(self):
-        return self.__new_balance
         return self.__amount_withdraw_or_deposit
 
     def get_new_balance(self):
+        return self.__new_balance
 
     def set_index(self, index):
         slef.__index = index
@@ -33,18 +36,39 @@ class Transaction:
 
 class BankAccout:
     
-    def __init__(self, first_name, last_name, initial_deposit):
+    def __init__(self, first_name, last_name, initial_deposit, password):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__curr_amount_of_money = initial_deposit
         self.__history_of_transactions = []
-    
+        self.__password_salt = __random_salt_generation(len(password))
+        self.__password = self.__hash_and_salt_password(password)
+        
     def __str__(self):
         return " " + self.__first_name + self.__last_name + " "
 
+    def __hash_and_salt_password(self, password):
+        hashed_password = 0
+        for index in (0, len(password)):
+            hashed_password += password[i] << i + self.__password_salt[i] << i
+        return hashed_password 
+
+    def __random_salt_generation(size, chars = string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
     def __save_transaction(self, is_deposit, amount):
         add = amount if is_deposit else -amount    
-        self.__history_of_transactions.append(Transaction(len(self.__history_of_transactions) + 1, is_deposit, amount, self.__curr_amount_of_money + add))
+        self.__history_of_transactions.append(Transaction(len(self.__history_of_transactions) + 1, is_deposit, amount, self.__curr_amount_of_money + add))\
+
+    def check_password(self, password):
+        curr_hashed_password = self.__hash_and_salt_password(password)
+        if curr_hashed_password == self.__password:
+            return True
+        return False
+
+    def change_password(self, password):
+        self.__password_salt = __random_salt_generation(len(password))
+        self.__password = self.__hash_and_salt_password(password)
 
     def withdraw(self, amount):
         if amount <= self.__curr_amount_of_money:
@@ -59,9 +83,7 @@ class BankAccout:
 
     def print_all_transactions(self):
         for curr_transaction in self.__history_of_transactions:
-            curr_transaction_type = "withdraw"
-            if curr_transaction.get_type():
-                curr_transaction_type = "deposit"
+            curr_transaction_type = "deposit" if curr_transaction.get_type() else "withdraw"
             print("transaction " + str(curr_transaction.get_index()) + " type - " + curr_transaction_type + " amount " + str(curr_transaction.get_amount_withdraw_or_deposit()) +  " new balance " +  str(curr_transaction.get_new_balance()))
 
     def set_first_name(self, name):
@@ -73,6 +95,10 @@ class BankAccout:
     def set_curr_amount_of_money(self, amount):
         self.__curr_amount_of_money = amount
 
+    def get_history_of_transactions(self, transactions):
+        self.__history_of_transactions = transactions
+
+
     def get_first_name(self):
         return self.__first_name
 
@@ -82,10 +108,9 @@ class BankAccout:
     def get_curr_amount_of_money(self):
         return self.__curr_amount_of_money
 
-my_bank_account = BankAccout("Radoslav", "Filipov", 200)
-my_bank_account.deposit(300)
-my_bank_account.deposit(1000)
-my_bank_account.withdraw(300)
-my_bank_account.deposit(1000)
-my_bank_account.withdraw(10000)
-my_bank_account.print_all_transactions()
+    def get_history_of_transactions(self):
+        return self.__history_of_transactions
+
+
+
+
